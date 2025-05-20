0f4c648be22a7454b0811d1aa9e8b884d9b631fa
cipherexx <biranimihir@gmail.com>
2025-05-20 21:56:25 +0000

    change structure for alternative deployment

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,8 @@
 import uvicorn
+import os
+
+port = os.getenv("PORT", 8000)
+
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
